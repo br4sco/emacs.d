ce7bacaea0eefdfcfe8702f07f390fcf7cc03f75
Oscar Eriksson <oscar.eriksson@protonmail.com>
2018-11-20 19:01:39 +0100

    fist commit

---
+++ init.el
@@ -0,0 +1,29 @@
+;;; init.el --- Where all the magic begins
+;;
+;;; Commentary:
+;; This file loads Org-mode and then loads the rest of the Emacs initialization from Emacs Lisp
+;; embedded in the literate Org-mode file: readme.org
+;;
+;;; Code:
+(require 'package)
+(setq package-enable-at-startup nil)
+(unless (assoc-default "melpa" package-archives)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+;; load up Org-mode and Org-babel
+(require 'org)
+(require 'ob-tangle)
+
+;; load up all literate org-mode files in this directory
+;; (mapc #'org-babel-load-file (directory-files emacs-dir t "\\.org$"))
+(org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
+
+;;; init.el ends here
