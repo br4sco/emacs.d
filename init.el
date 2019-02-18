bb78cfc610a8ad39fa651a86f8b48b396aa47853
Oscar Eriksson <oerikss@kth.se>
2019-02-18 11:05:24 +0100

    Use straight

--- init.el
+++ init.el
@@ -5,16 +5,32 @@
 ;; embedded in the literate Org-mode file: readme.org
 ;;
 ;;; Code:
-(require 'package)
-(setq package-enable-at-startup nil)
-(unless (assoc-default "melpa" package-archives)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
-(package-initialize)
+;; (require 'package)
+;; (setq package-enable-at-startup nil)
+;; (unless (assoc-default "melpa" package-archives)
+  ;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
+;; (package-initialize)
 
 ;; Bootstrap use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+  ;; (package-refresh-contents)
+  ;; (package-install 'use-package))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 (setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
