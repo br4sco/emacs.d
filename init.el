9e8f219fa04b7ad6754cf7b345964e6b323846d4
Oscar Eriksson <oscar.eriksson@protonmail.com>
2019-04-03 09:15:40 +0200

    Add lisp dir to whitelist and update init.el

--- init.el
+++ init.el
@@ -1,33 +1,25 @@
-;;; init.el --- Where all the magic begins
-;;
-;;; Commentary:
-;; This file loads Org-mode and then loads the rest of the Emacs initialization from Emacs Lisp
-;; embedded in the literate Org-mode file: readme.org
-;;
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(add-to-list 'load-path (concat user-emacs-directory "lisp/"))
 
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+(require 'init-packages)
+(require 'init-misc)
+(require 'init-appearance)
+(require 'init-editing)
+(require 'init-dired)
+(require 'init-navigation)
+(require 'init-tramp)
+(require 'init-compile)
+(require 'init-modelyze)
+(require 'init-keybindings)
 
-(setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(use-package diminish)
+(use-package bind-key)
 
-;; load up Org-mode and Org-babel
-(require 'org)
-(require 'ob-tangle)
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
 
-;; load up all literate org-mode files in this directory
-;; (mapc #'org-babel-load-file (directory-files emacs-dir t "\\.org$"))
-(org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
-
-;;; init.el ends here
+(use-package editorconfig
+  :diminish
+  (editorconfig-mode)
+  :config
+  (editorconfig-mode 1))
